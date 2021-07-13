5034b9dedda1f188e8be0c94bdb9548a710dc1ee
propan2one_manjaro <jeandelmotte0392@gmail.com>
2021-07-13 16:01:50 +0200

    feat: basic function to generate random DNA sequence

--- src/insilicogenome/insilicogenome.py
+++ src/insilicogenome/insilicogenome.py
@@ -1,7 +1,35 @@
-import pandas as pd
+from Bio.Seq import Seq
+import numpy as np
 
+def random_dnasequence(size):
+    """
+    Generate random sequence of DNA
 
-def catbind(a, b):
-    concatenated = pd.concat([pd.Series(a.astype("str")),
-                              pd.Series(b.astype("str"))])
-    return pd.Categorical(concatenated)
+    Parameters
+    ----------
+    size : Size of DNA in bp
+      An integer between 1 and 100 000 000.
+
+    Returns
+    -------
+    pandas.core.arrays.categorical.Categorical
+      The new concatenated pandas categorical.
+
+    Examples
+    --------
+    >>> from insilicogenome import insilicogenome
+    >>> insilicogenome.random_dnasequence(10)
+    Seq('GTTCTTGAT')
+    """
+    if ((size>= 0) and (size<= 100000000)):
+        pass  # empty file
+    elif (size > 100000000):  # on a title line
+        raise ValueError(
+            "The size of the sequence to generate is greater than the maximum size of 100000000bp"
+            f"The value of '{size}' is too big, it must be reduced"
+        )
+    else:
+        assert size > 0, "Size value negatif ; this should be impossible!"
+    np.random.seed(123)
+    sequence = ''.join(np.random.choice(('C','G','T','A'), size ))
+    return Seq(sequence)
