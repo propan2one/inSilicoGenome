d5d9af82c4d7382350e9463962701748c38a0d9e
propan2one_manjaro <jeandelmotte0392@gmail.com>
2021-07-13 16:54:32 +0200

    file: unit test of random_dnasequence

--- tests/test_insilicogenome.py
+++ tests/test_insilicogenome.py
@@ -1,2 +1,11 @@
 from insilicogenome import __version__
 from insilicogenome import insilicogenome
+from Bio.Seq import Seq
+import numpy as np
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+def test_random_dnasequence():
+    size = 10
+    assert ((insilicogenome.random_dnasequence(size)).codes == Seq('GTTCTTGAT')).all()
